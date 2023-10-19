47a52093e9aaa2c0434ee2bcfb0241e3b1e6f12c
Humberto Viveros <humberto.viverosrios@gmail.com>
2023-10-19 14:28:42 -0300

    BD, Migrations, Model, Controller

---
+++ app/Http/Controllers/PersonaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Persona;
+use Illuminate\Http\Request;
+
+class PersonaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Página de inicio
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // Formulario de creación
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Inserción de datos
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Persona $persona)
+    {
+        // Detalles de un registro
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Persona $persona)
+    {
+        // Formulario de edición
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Persona $persona)
+    {
+        // Actualización de un registro
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Persona $persona)
+    {
+        // Elimina un registro
+    }
+}
